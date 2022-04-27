010227a84bbcaa81f22e751897fe6156e5a72e2e
mdk <mathieu.dekeyzer@smals.be>
2022-04-27 23:25:05 +0200

    feat: add plausible

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,7 @@
 import adminMenu from '@elasticms/admin-menu';
 import '../css/app.scss';
 import {drawioPreviewer} from './module/drawio'
+import Plausible from 'plausible-tracker'
 
 adminMenu();
 
@@ -16,6 +17,22 @@ document.addEventListener('ems-preview', function (event){
     console.log('ems-preview loaded!');
     drawioPreviewer();
 });
+
+const { trackPageview } = Plausible({
+    domain: 'mathieu.dekeyzer.net',
+    apiHost: 'https://analytics.theus.be'
+})
+
+trackPageview();
+
+const assetLinks = document.querySelectorAll("a[href$='.pdf']");
+for (let i = 0; i < assetLinks.length; i++) {
+    assetLinks[i].addEventListener("click", function(){
+        trackPageview({
+            url: this.href
+        });
+    });
+}
 
 (function() {
     console.log('CV website loaded!');
